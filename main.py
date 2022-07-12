2ae15d4c95e09573cc1095b19f67bf1754fa71e2
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2022-07-12 18:54:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def say_list(message):
     logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used /start or /help')
     bot.send_message(
         message.chat.id,
-        '<b>Prise list <a href="https://github.com/D1MONSHT/bot/issues/1#issue-1302247098"></a></b>',
+        '<b>Prise list <a href="https://github.com/D1MONSHT/bot"></a></b>',
         parse_mode='html'
     )
 
