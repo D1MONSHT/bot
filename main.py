20c51e4c3a5e8506315f3f8722102a8f322cda47
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2022-07-12 21:38:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,32 +28,75 @@ dialog = {
 
 
 # --------------- bot -------------------
-@bot.message_handler(commands=['help', 'start'])
-def say_welcome(message):
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used /start or /help')
+
+user_dict = {}  # {1234567890: {'photo': photo_id, 'caption': some_text}}
+
+
+def start_keyboard():
+    keyboard = types.InlineKeyboardMarkup()
+    send = types.InlineKeyboardButton(text='send photo', callback_data='send')
+    keyboard.add(send)
+    return keyboard
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
     bot.send_message(
         message.chat.id,
-        '<b>Hello! This is a telegram bot template written by <a href="https://github.com/otter18">otter18</a></b>',
-        parse_mode='html'
-    )
-@bot.message_handler(commands=['list'])
-def say_list(message):
-  bot.send_photo(
-        message.chat.id,
-        '<b><a href="https://static.mk.ru/upload/entities/2020/05/22/16/photoreportsImages/detailPicture/f5/11/0b/41/6512506_5490947.jpg"></a></b>',
-        parse_mode='photo'
+        'Выберите действие ⤵', reply_markup=start_keyboard()
     )
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
-            bot.send_message(message.chat.id, random.choice(resp['out']))
-            return
 
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
-    bot.send_message(message.chat.id, message.text)
+@bot.callback_query_handler(func=lambda call: call.data == 'send')
+def admin_send(call):
+    bot.send_message(call.from_user.id, 'Отправьте картинку.')
+    bot.register_next_step_handler(call.message, photo_handler)
+
+
+def photo_handler(message):
+    try:
+        # если изображение есть в словаре - заменяем его и убираем описание
+        if user_dict.get(message.chat.id) is not None:
+            user_dict[message.chat.id]['photo'] = message.photo[len(message.photo) - 1].file_id
+            user_dict[message.chat.id]['caption'] = ''
+        else:
+            # если фото нет - создаем словарь и добавляем изображение
+            user_dict[message.chat.id] = {'photo': '', 'caption': ''}
+            user_dict[message.chat.id]['photo'] = message.photo[len(message.photo) - 1].file_id
+    except Exception as e:
+        bot.reply_to(message, e)
+    else:
+        bot.send_message(message.chat.id, 'Теперь введите текст к картинке')
+        bot.register_next_step_handler(message, text_handler)
+
+
+def text_handler(message):
+    # добавляем описание изображения фото в словарь
+    user_dict[message.chat.id]['caption'] = message.text
+    bot.send_message(message.chat.id, 'Напишите "отправить" чтобы разослать картинку пользователям')
+    bot.register_next_step_handler(message, send_to_users)
+
+
+def send_to_users(message):
+    if message.text.lower() == 'отправить':
+        # здесь можно добавить отправку в цикле      
+        bot.send_photo(
+            message.chat.id,
+            photo=user_dict[message.chat.id]['photo'],
+            caption=user_dict[message.chat.id]['caption'],
+            parse_mode='HTML'
+        )
+    else:
+        bot.send_message(message.chat.id, 'Напишите "отправить" чтобы разослать картинку пользователям')
+        bot.register_next_step_handler(message, send_to_users)
+
+
+bot.polling(none_stop=True, interval=0)
+Последнее редактирование: Янв 13, 2022
+Мне нравится Реакции:rosvo
+ 0 
+rosvo
+rosvo
 
 
 if __name__ == '__main__':
