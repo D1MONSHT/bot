b5acee789275ebe8f3d7e5f37d2cb7734fc6ecae
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2022-07-12 12:09:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,6 @@ import random
 from setup import bot, logger
 from webhook import app
 
-# --------------- dialog params -------------------
-dialog = {
-    'hello': {
-        'in': ['привет', 'hello', 'hi', 'privet', 'hey'],
-        'out': ['Приветствую', 'Здравствуйте', 'Привет!']
-    },
-    'how r u': {
-        'in': ['как дела', 'как ты', 'how are you', 'дела', 'how is it going'],
-        'out': ['Хорошо', 'Отлично', 'Good. And how are u?']
-    },
-    'name': {
-        'in': ['зовут', 'name', 'имя'],
-        'out': [
-            'Я telegram-template-bot',
-            'Я бот шаблон, но ты можешь звать меня в свой проект',
-            'Это секрет. Используй команду /help, чтобы узнать'
-        ]
-    }
-}
-
-
 # --------------- bot -------------------
 
 @bot.message_handler(commands="start")
@@ -37,16 +16,6 @@ async def cmd_start(message: types.Message):
     await message.answer("Как подавать котлеты?", reply_markup=keyboard)
 
 
-
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
-            bot.send_message(message.chat.id, random.choice(resp['out']))
-            return
-
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
     bot.send_message(message.chat.id, message.text)
 @bot.message_handler(commands=["id"])
 
