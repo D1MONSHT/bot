9397dba7bd72b08e7ca93c3b8c727e3ef573acc9
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2022-07-12 19:19:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,13 +40,9 @@ def say_welcome(message):
 
 def say_list(message):
     logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used /start or /help')
-    bot.send_message(
-        message.chat.id,
-        '<b>Price <href="https://github.com/D1MONSHT/bot"></b>',
-        parse_mode='html'
-    )
+    bot.send_photo(message.chat.id, 'https://www.google.com/search?q=%D1%84%D0%BE%D1%82%D0%BE&sxsrf=ALiCzsaYhI-PVwB3DHMiFOEcxi1fMVFJXQ:1657642627491&tbm=isch&source=iu&ictx=1&vet=1&fir=zKA8Ct0R5qPKGM%252C0VIOfvdZkqu73M%252C_%253BtjaOjcbyWAenTM%252Cqv3bYgAKOmFK9M%252C_%253BkQ4ie-ohy9OThM%252CTk1lAhrAMtACXM%252C_%253BHgZWKUUFpwmrIM%252C989fVJpn7S7e0M%252C_%253Bszq9pXtD1OugNM%252CUf0Kd7wb6NMOQM%252C_%253BfK7SylISiIx1-M%252CVTiWAQsfu3ZyUM%252C_%253BO3oosmQ_Hg1o-M%252Cs-3xaS9p91GbvM%252C_%253BSmaLNhjYJWX97M%252CymXz8zPLcPB7rM%252C_%253BeH6xhsVRGRkUsM%252CcyM20HpU-UzH6M%252C_%253BZOnJnqXnI1IqwM%252Cw8OFW9wEubEiLM%252C_%253BFd2HV93btDH6YM%252Cs-3xaS9p91GbvM%252C_%253B6TZUCyv2nUgKgM%252CSTlVqyv8LyMZTM%252C_%253BhjWMM1IOTHoVcM%252CTk1lAhrAMtACXM%252C_&usg=AI4_-kRAjTLp6DbQwODd_EVKp0jQBz7PWQ&sa=X&ved=2ahUKEwjjmuf_3_P4AhUSSxoKHWTzCtAQ9QF6BAgJEAE&biw=1366&bih=625&dpr=1#imgrc=eH6xhsVRGRkUsM')
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     if os.environ.get("IS_PRODUCTION", "False") == "True":
         app.run()
     else:
